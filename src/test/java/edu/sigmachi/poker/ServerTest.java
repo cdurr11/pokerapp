3bfcd179468c7bd742b934fc1fb80661d8ce46f4
Cody Durr <cdurr@Codys-MacBook-Pro.local>
2020-04-21 01:04:12 -0400

    added test skeleton

---
+++ src/test/java/edu/sigmachi/poker/ServerTest.java
@@ -0,0 +1,13 @@
+package edu.sigmachi.poker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerTest {
+  
+  @Test
+  public void test1() {
+    assertTrue(true);
+  }
+}
