0f4010e5a38daf57c2602fdbee8f74594d53254a
cdurr <cdurr619@gmail.com>
2020-05-11 23:30:01 -0400

    layed out the start method

--- server/src/main/java/edu/sigmachi/poker/Game.java
+++ server/src/main/java/edu/sigmachi/poker/Game.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 import com.corundumstudio.socketio.SocketIOServer;
 
+import edu.sigmachi.poker.ConsoleMessages.ConsoleMsg;
+
 public class Game {
   private final String gamePassword;
   private Set<String> allPlayersEver;
@@ -15,17 +17,31 @@ public class Game {
   private Map<String, String> playersToCards;
   private final Queue<LoginAttemptMsg> loginMsgQueue;
   private final Queue<ClientActionMsg> clientMsgQueue;
+  private final Queue<ConsoleMsg> consoleMsgQueue;
   
   
   public Game(SocketIOServer server, String gamePassword, Queue<LoginAttemptMsg> loginMsgQueue, 
-      Queue<ClientActionMsg> clientMsgQueue) {
+      Queue<ClientActionMsg> clientMsgQueue, Queue<ConsoleMsg> consoleMsgQueue) {
     this.loginMsgQueue = loginMsgQueue;
     this.clientMsgQueue = clientMsgQueue;
     this.gamePassword = gamePassword;
+    this.consoleMsgQueue = consoleMsgQueue;
   }
   
-  
-  
+  public void start() {
+    while (true) {
+      //check and handle login requests here
+      table.addPlayer();
+      table.removePlayer();
+      //handle consoleMessages here
+        // Modify table however console messages say
+      
+      
+      //perform a round
+      table.doRound(server, clientMsgQueue)
+      //send message to the front-end
+    }
+  }
   
   /**
    * Authenticate and add the player to the game
